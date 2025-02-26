9bd9cc2ee61fea463600e7b3b0374b5b85ee7fb3
Acearoni <awoodson.contact@gmail.com>
2025-02-25 22:42:17 -0600

    Full Crud

--- server/controllers/game.controller.js
+++ server/controllers/game.controller.js
@@ -11,25 +11,46 @@ module.exports = {
                 res.status(500).json(err)
             })
     },
-// //Read
-//     findOneGame: (req, res) => {
-//         Game.findOne({_id: req.params.id})
-//             .then((oneGame) => {
-//                 res.status(200).json(oneGame)
-//             })
-//             .catch((err) => {
-//                 res.status(500).json(err)
-//             })
-//     },
-//     findAllGames: (req, res) => {
-//         Game.find()
-//             .then((allGames) => {
-//                 res.status(200).json(allGames)
-//             })
-//             .catch((err) => {
-//                 console.log("Something Went Wrong When Finding All")
-//                 res.status(500).json(err)
-//             })
-//     }
-// }
+//Read
+    findOneGame: (req, res) => {
+        Game.findOne({_id: req.params.id})
+            .then((oneGame) => {
+                res.status(200).json(oneGame)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+    findAllGames: (req, res) => {
+        Game.find()
+            .then((allGames) => {
+                res.status(200).json(allGames)
+            })
+            .catch((err) => {
+                console.log("Something Went Wrong When Finding All")
+                res.status(500).json(err)
+            })
+    },
+//Update
+updateGame: (req, res) => {
+    Game.findOneAndUpdate({ _id: req.params.id },
+        req.body,
+        { new: true, runValidators: true })
+        .then((updatedGame) => {
+            res.json(updatedGame)
+        })
+        .catch((err) => {
+            res.status(500).json(err)
+        })
+},
+    //Delete
+    deleteGame: (req,res) => {
+        Game.deleteOne({_id: req.params.id})
+            .then((result) => {
+                res.status(201).json(result)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    }
 }
--- server/routes/game.routes.js
+++ server/routes/game.routes.js
@@ -2,4 +2,8 @@ const GameController = require('../controllers/game.controller')
 
 module.exports = (app) => {
     app.post('/api/createGame', GameController.createGame)
+    app.get('/api/findOneGame/:id', GameController.findOneGame)
+    app.get('/api/findAllGames', GameController.findAllGames)
+    app.put('/api/updateGame/:id', GameController.updateGame)
+    app.delete('/api/deleteGame/:id', GameController.deleteGame)
 }
